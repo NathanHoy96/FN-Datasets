ea5f606610c436970beaab180a769711136a10d3
NathanHoy96 <nathan_hoy@live.co.uk>
2022-05-23 00:31:23 +0100

    KDNuggets - changed FAKE/REAL labels to 0/1 to match other datasets

--- dataset-clean.py
+++ dataset-clean.py
@@ -23,3 +23,10 @@ Kaggle_dataset_true['label'] = 1
 
 Kaggle_merged = pd.concat([Kaggle_dataset_fake,Kaggle_dataset_true])
 print(Kaggle_merged.head())
+
+#KDNuggets Dataset, change FAKE/REAL labels to 0/1
+
+KDNuggets_dataset = pd.read_csv(".\\Datasets\\Original Downloads\\KDNuggets\\fake_or_real_news.csv")
+KDNuggets_dataset['label'] = KDNuggets_dataset['label'].str.replace("FAKE","0")
+KDNuggets_dataset['label'] = KDNuggets_dataset['label'].str.replace("REAL","1")
+print(KDNuggets_dataset.head())
