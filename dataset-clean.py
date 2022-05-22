928632aeb3fd3748012242e141f31f2a43857fb2
NathanHoy96 <nathan_hoy@live.co.uk>
2022-05-23 00:22:59 +0100

    Merged Kaggle Fake and Real dataset. Appears to be identical to ISOT dataset

--- dataset-clean.py
+++ dataset-clean.py
@@ -8,11 +8,18 @@ ISOT_dataset_true = pd.read_csv(".\\Datasets\\Original Downloads\\ISOT\\True.csv
 ISOT_dataset_fake['label'] = 0
 ISOT_dataset_true['label'] = 1
 
-print(ISOT_dataset_fake.head())
-print(ISOT_dataset_true.head())
-
-ISOT_pair = [ISOT_dataset_true,ISOT_dataset_fake]
-
+ISOT_pair = [ISOT_dataset_fake,ISOT_dataset_true]
 ISOT_merged = pd.concat(ISOT_pair)
 
-print(ISOT_merged.tail())
+print(ISOT_merged.head())
+
+#Fake and Real News Kaggle Dataset
+
+Kaggle_dataset_fake = pd.read_csv(".\\Datasets\\Original Downloads\\Fake and real news dataset (Kaggle)\\Fake.csv")
+Kaggle_dataset_true = pd.read_csv(".\\Datasets\\Original Downloads\\Fake and real news dataset (Kaggle)\\True.csv")
+
+Kaggle_dataset_fake['label'] = 0
+Kaggle_dataset_true['label'] = 1
+
+Kaggle_merged = pd.concat([Kaggle_dataset_fake,Kaggle_dataset_true])
+print(Kaggle_merged.head())
